a671ba7eda4239f9fbe533ac2064519a3b248ed6
Divy Barot <divybarot15@gmail.com>
2026-03-09 16:41:30 +0530

    footer-added

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Gallery from '@/components/ui/Gallery'
 import SectionCards from '@/components/ui/SectionCards'
 import Milestones from '@/components/ui/Milestones'
 import Partners from '@/components/ui/Partners'
+import Footer from '@/components/ui/Footer'
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <SectionCards />
       <Milestones />
       <Partners />
+      <Footer />
       <div style={{ height: '100vh', background: '#080808' }} />
     </main>
   )
